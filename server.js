a8780c5e9b15bb2de5c5ed23e2c43eb89dbc5305
abdoujammeh64 <abdoujammeh64@gmail.com>
2021-04-23 13:17:20 -0400

    modified server file

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ const myApp = require("./vendor/framework");
 
 require("./routes/api");
 
+const path = require("path");
+const express = require("express");
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+  return res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 myApp.listen();
